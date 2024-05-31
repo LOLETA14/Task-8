e60b292aafcf0ce3f5f7c895922a034eb2fa3430
DELL <lourinalolo675@gmail.com>
2024-05-31 21:24:36 +0300

    UPdate3

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,9 +1,9 @@
 export default function Cards() {
     let card =[
-        {h2:"Basic",pargraph:"for very small businessesand freelancers.",h22:"$2.5/mo",li:[`Disk Space:500 M.B`,`Bandwidth:500 G.B`,`SubDomains: Unlimited`,`Parked Domains: Unlimited`,`E-mail Accounts: Unlimited`,`Cpanel (control panel): Yes`,`Money Back Guarantee: 14 Days`],button:`You will have 1 Gift`}
-        ,{h2:"Pro",pargraph:"for Individuals that need sharingvlogs or need huge portfolio.",h22:"$5/mo",li:[`Disk Space:3000 M.B`,`Bandwidth:3 TERA`,`SubDomains: Unlimited`,`Parked Domains: Unlimited`,`E-mail Accounts: Unlimited`,`Cpanel (control panel): Yes`,`Money Back Guarantee: 14 Days`],button:`1 Gift per/mo for year`},
-        {h2:"Business",pargraph:"for Team that need sharingand reporting.",h22:"$8/mo",li:[`Disk Space:5000 M.B`,`Bandwidth:5 TERA`,`SubDomains: Unlimited`,`Parked Domains: Unlimited`,`E-mail Accounts: Unlimited`,`Cpanel (control panel): Yes`,`Money Back Guarantee: 14 Days`],button:`3 Gifts per/mo for year`},
-        {h2:"Enterprise",pargraph:"for large companies that needadmins & security",h22:"$12/mo",li:[`Disk Space:10000 M.B`,`Bandwidth: 10 TERA`,`SubDomains: Unlimited`,`Parked Domains: Unlimited`,`E-mail Accounts: Unlimited`,`Cpanel (control panel): Yes`,`Money Back Guarantee: 14 Days`],button:`Just Call Us`}
+        {h2:"Basic",pargraph:"for very small businessesand freelancers.",h22:"$2.5/mo",par:"",li:[`Disk Space:500 M.B`,`Bandwidth:500 G.B`,`SubDomains: Unlimited`,`Parked Domains: Unlimited`,`E-mail Accounts: Unlimited`,`Cpanel (control panel): Yes`,`Money Back Guarantee: 14 Days`],button:`You will have 1 Gift`}
+        ,{h2:"Pro",pargraph:"for Individuals that need sharingvlogs or need huge portfolio.",h22:"$5/mo",par:"",li:[`Disk Space:3000 M.B`,`Bandwidth:3 TERA`,`SubDomains: Unlimited`,`Parked Domains: Unlimited`,`E-mail Accounts: Unlimited`,`Cpanel (control panel): Yes`,`Money Back Guarantee: 14 Days`],button:`1 Gift per/mo for year`},
+        {h2:"Business",pargraph:"for Team that need sharingand reporting.",h22:"$8/mo",par:"5% discount durin 2022",li:[`Disk Space:5000 M.B`,`Bandwidth:5 TERA`,`SubDomains: Unlimited`,`Parked Domains: Unlimited`,`E-mail Accounts: Unlimited`,`Cpanel (control panel): Yes`,`Money Back Guarantee: 14 Days`],button:`3 Gifts per/mo for year`},
+        {h2:"Enterprise",pargraph:"for large companies that needadmins & security",h22:"$12/mo",par:"8% discount durin 2022",li:[`Disk Space:10000 M.B`,`Bandwidth: 10 TERA`,`SubDomains: Unlimited`,`Parked Domains: Unlimited`,`E-mail Accounts: Unlimited`,`Cpanel (control panel): Yes`,`Money Back Guarantee: 14 Days`],button:`Just Call Us`}
     ]
  return(
         <>
@@ -16,7 +16,10 @@ export default function Cards() {
                     <div className="card-son1">
                     <h4>{element.h2}</h4>
                     <p>{element.pargraph}</p>
+                    <div className="adddiscount">
                       <h2>{element.h22}</h2>
+                      <span className="discount">{element.par}</span>
+                      </div>
                       <ul>
                         <li>{element.li[0]}</li>
                         <li>{element.li[1]}</li>
